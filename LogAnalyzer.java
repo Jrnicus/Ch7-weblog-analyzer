d83423f567491f2cc45ad5c666da2d97b188b80a
Jrnicus <jrnicus@gmail.com>
2019-10-14 12:31:23 -0400

    Added numberOfAccesses and busiestHour method
    numberOfAccesses will return the number of logs in a file. busiestHour will print the hour with the most logs.

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -2,7 +2,9 @@
  * Read web server data and analyse hourly access patterns.
  * 
  * @author David J. Barnes and Michael Kölling.
- * @version    2016.02.29
+ * 
+ * @author Steve Cate
+ * @version    10/14/19
  */
 public class LogAnalyzer
 {
@@ -21,6 +23,21 @@ public class LogAnalyzer
         hourCounts = new int[24];
         // Create the reader to obtain the data.
         reader = new LogfileReader();
+    }
+    
+    /**
+     * Create an object to open a certain file
+     */
+    public LogAnalyzer(String fileName)
+    {
+        hourCounts = new int[24];
+        
+        reader = new LogfileReader(fileName);
+        
+        // We can fill out the array list right away because we have
+        // all the data in the file
+        analyzeHourlyData();
+        
     }
 
     /**
@@ -54,5 +71,60 @@ public class LogAnalyzer
     public void printData()
     {
         reader.printData();
+    }
+    
+    /**
+     * This will add all the logs from each hour to get a total
+     * @return total number of logs
+     * 
+     */
+    public int numberOfAccesses()
+    {
+        int total = 0;
+        for(int i=0; hourCounts.length > i; i++)
+        {
+            total += hourCounts[i];
+        }
+        
+        return total;
+    }
+    
+    /**
+     * This will look at all the logs and let you know what
+     * the busiest hour or hours are.
+     */
+    public void busiestHour()
+    {
+        int theBusiestHour = 0;
+        boolean multipleBusiestHours = false;
+        
+        for(int i=1; hourCounts.length > i; i++)
+        {
+            if (hourCounts[i] == theBusiestHour)
+            {
+                multipleBusiestHours = true;
+            }
+            if (hourCounts[i] > hourCounts[theBusiestHour])
+            {
+                theBusiestHour = i;
+            }
+        }
+        
+        if (multipleBusiestHours)
+        {
+            System.out.print("The Busiest Hours are ");
+            for(int i=0; hourCounts.length > i; i++)
+            {
+                if(hourCounts[1] == hourCounts[theBusiestHour])
+                {
+                    System.out.print(i + ", ");
+                }
+            }
+            System.out.println();
+        }
+        else
+        {
+            System.out.println("The Busiest Hour is " + theBusiestHour);
+        }
     }
 }
